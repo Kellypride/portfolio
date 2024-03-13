4ce802676a9ed6a2eca746bfd973a8b64d9517b8
kellypride <ngehkellypride181@gmail.com>
2024-03-13 17:18:18 +0100

    Set up my-porfolio project environment using tailwindcss and react.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-200 p-48">
+      <h1 className="text-3xl text-purple-800">Hello Tailwindcss!</h1>
+      <p className="text-gray-700 mt-4">Let's Build Something Amazing</p>
     </div>
   );
 }
